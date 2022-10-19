3753bd3b233feee28b0eac2a22553d4ae1a27032
aaarunan <arunan2212@gmail.com>
2022-10-19 22:44:14 +0200

    Try list instead

--- oblig7/oppgave7.py
+++ oblig7/oppgave7.py
@@ -1,87 +1,143 @@
 from collections import defaultdict
-import random
+from dataclasses import dataclass
+from math import dist
 
+@dataclass
+class Edge:
+    start: int
+    end: int
+    weight: int
+
+@dataclass
+class Node:
+    value : int
+    edges : list[Edge]
+
+    def append_edge(self,end: int, weight: int) -> None:
+        self.edges.append(Edge(self.value, end, weight))
+
+
+class Graph:
+    def __init__(self, nodes: int, edges : list[Edge]) -> None:
+        self.nodes = nodes
+        self.edges = edges
+        self.graph = [None] * self.nodes
+    
+
+    def add(self, start: int, end: int, weight: int) -> None:
+        if self.graph[start] is not None:
+            self.graph[start].append_edge(end, weight)
+            return
+        self.graph[start] = Node(start, [Edge(start, end,weight)])
+
+    def dijikstras(self, start: int):
+        distances = [float("inf")] * self.nodes
+        distances[start] = 0
+        predecessors = [None] * self.nodes
+        queue = PriorityQueue()
+        queue.insert(self.graph[start], 0)
+        visited = set()
+        for node in self.graph:
+            if node == None:
+                continue
+            if node.value != start:
+                queue.insert(node, float("inf"))
+        
+        while not queue.is_empty(): 
+            current_node, distance = queue.peek()
+            if current_node.value in visited:
+               continue
+            visited.add(current_node.value)
+            for edge in current_node.edges:
+                if edge.end not in visited:
+                    new_weight = distance + edge.weight
+                    queue.insert(, new_weight)
+            distances[current_node.value] = distance
+            predecessors[current_node.value] = current_node
+        
+        print(distances)
+            
+                   
+
+    def __repr__(self) -> str:
+        return str(self.graph)
 
 class PriorityQueue:
-    queue = []
-
-    def __init__(self) -> None:
-        pass
+    distances = []
+    nodes = []
 
     def swap(self, i, j):
-        self.queue[i], self.queue[j] = self.queue[j], self.queue[i]
+        self.distances[i], self.distances[j] = self.distances[j], self.distances[i]
+        self.nodes[i], self.nodes[j] = self.nodes[j], self.nodes[i]
+
+    def is_empty(self):
+        if len(self.nodes) == 0:
+           return True 
+
+        return False
+    
+    def update_node(self, node, weight):
+        self.distances[self.nodes[node].value] = weight
+        self.heapify(0, len(self.nodes))
+    
+    def get_distance(self, node):
+        print(self.nodes)
+        print(node)
+        return self.distances[self.nodes[node].value]
 
     def heapify(self, end):
         if end != 0:
             for i in range((end // 2) - 1, -1, -1):
                 self.heapify_util(i, end)
 
-    def heapify_util(self, i, end):
-        largest = i
+    def heapify_util(self, i, end: Node):
+        smallest = i
 
         left = 2 * i + 1
         right = 2 * i + 2
 
-        if left < end and self.queue[left] > self.queue[i]:
-            largest = left
+        if left < end and self.distances[left] < self.distances[i]:
+            smallest = left
 
-        if right < end and self.queue[right] > self.queue[i]:
-            largest = right
+        if right < end and self.distances[right] < self.distances[i]:
+            smallest = right
 
-        if largest != i:
-            self.swap(largest, i)
+        if smallest != i:
+            self.swap(smallest, i)
 
-        if right < end and self.queue[left] > self.queue[i]:
+        if right < end and self.distances[left] < self.distances[i]:
             self.swap(left, i)
 
-    def insert(self, num):
-        self.queue.append(num)
-        self.heapify(len(self.queue))
-
+    def insert(self, node, weight):
+        self.nodes.append(node)
+        self.distances.append(weight)
+        self.heapify(len(self.nodes))
+    
+        
     def peek(self):
-        if len(self.queue) == 0:
+        if len(self.nodes) == 0:
             return None
 
-        peek = self.queue.pop(0)
-        self.heapify(len(self.queue))
-        return peek
-
-
-class Graph:
-    def __init__(self, nodes: int) -> None:
-        self.nodes = nodes
-        self.graph = defaultdict(set)
-
-    def add(self, start: int, end: int) -> None:
-        self.graph[start].add(end)
-
-    def dijikstras(self):
-        return
+        peek = self.nodes.pop(0)
+        distance = self.distances.pop(0)
+        self.heapify(len(self.nodes))
+        return [peek, distance]
 
 
 def main():
-
+    graph = parse_from_file()
+    print(graph.dijikstras(1))
     return
 
+def parse_from_file():
+    with open("vg1.txt", "r", encoding="UTF-8") as file:
+        lines = file.read().splitlines()
+    args = lines[0].split()
+    graph = Graph(int(args[0]), int(args[1]))
+    for line in lines[1:]:
+        values = line.split()
+        graph.add(int(values[0]), int(values[1]), int(values[2]))
+    return graph
 
 if __name__ == "__main__":
     main()
-
-
-def test_peek():
-
-    nums = random.sample(range(-100000, 100000), 5000)
-    queue = PriorityQueue()
-
-    for num in nums:
-        queue.insert(num)
-
-    peek = queue.peek()
-    result = []
-    while peek:
-        result.append(peek)
-        peek = queue.peek()
-
-    assertion = result[:]
-    assertion.sort()
-    assert result == assertion[::-1]
